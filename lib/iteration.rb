6655c5f983bb8727e19fb4841a01c09223450ffe
Joshua Gauthreaux <joshuagauthreaux@gmail.com>
2020-02-17 23:39:20 +0000

    Done.

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -1,4 +1,5 @@
 require 'pry'
+# binding.pry
 
 def join_ingredients(src)
   # Given an Array of 2-element Arrays ( [ [food1, food2], [food3, # food4]....[foodN, foodM]]):
@@ -26,27 +27,12 @@ def find_greater_pair(src)
   higher_number = []
   row_index = 0
 while row_index < src.count do
-  element_index = 0
   higher_number.push(src[row_index].max)
   row_index += 1
 end
 higher_number  
 end
 
-#   counter = 0
-#   max_value = 0
-#   array = [] 
-# while counter < src.count do 
-#   src.each do |i|
-#     if i > max_value
-#       max_value = i
-#       end
-#     array << i
-#     counter += 1
-#   end
-#   array
-# end
-# binding.pry
 
 
 def total_even_pairs(src)
@@ -57,4 +43,41 @@ def total_even_pairs(src)
   # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
   # the number was even. Review the operator documentation if you've forgotten
   # this!
+  sum_even = []
+  row_index = 0
+while row_index < src.count do 
+ if src[row_index][0] % 2 == 0 && src[row_index][1] % 2 == 0 then
+   sum_even << src[row_index].sum
 end
+row_index += 1  
+end
+sum_even.sum
+end
+
+
+
+
+
+
+
+
+
+
+# def total_even_pairs(src)
+#   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
+#   # if both numbers in the pair are even, then add both those numbers to the
+#   # total
+#   #
+#   # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
+#   # the number was even. Review the operator documentation if you've forgotten
+#   # this!
+#   sum_even = []
+#   row_index = 0
+# while row_index < src.count do 
+# if src[row_index].sum % 2 == 0 then
+#   sum_even << src[row_index].sum
+# end
+# row_index += 1  
+# end
+# sum_even.sum
+# end
